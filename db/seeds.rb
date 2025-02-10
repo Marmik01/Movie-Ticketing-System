ad1dab25d3772efb599e761f185754d759b84944
mpatel28 <mpatel28@ncsu.edu>
2025-02-10 18:36:11 -0500

    created admin user
    updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  username: "admin",
+  name: "Admin user",
+  email: "admin@admin.com",
+  password: "admin123",
+  is_admin: true
+)
