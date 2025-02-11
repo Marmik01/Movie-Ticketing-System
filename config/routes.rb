742747540372e5e6641632937d60fed56fd9f9a0
mpatel28 <mpatel28@ncsu.edu>
2025-02-11 15:02:57 -0500

    fixed logout problem

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"  # Temporary GET logout route
   delete "logout", to: "sessions#destroy"
 
   resources :tickets
